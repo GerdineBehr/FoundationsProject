e5ef2c72ea16b0d3a4b4de30043f0956d9436b2b
Gerdinebehr <gerdine672@revature.net>
2024-09-18 22:27:32 -0400

    created gitignore and started DAO, and created ENV for Foundations project

---
+++ paybackDAO.js
@@ -0,0 +1,37 @@
+
+/*
+Reimbursement info:
+-Amount
+-Description
+-Status
+-Employee ID 
+ 
+ 
+ 
+ // USER FACING 
+
+
+// POST Create new Username and password 
+
+// GET Read if username exists
+
+// POST Login User 
+
+// POST Create Reimbursement request 
+
+// GET Read status of reimbursement request 
+
+//
+
+
+// ADMIN FACING 
+
+// POST Create new Username and password 
+
+// GET Read if username exists
+
+// POST login 
+
+// GET Read Reimbursement request 
+
+// PUT Update Reimbursement ticket to approved/denied;*/
