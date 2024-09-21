f0655a6510c8e334ede462edd6ef21e4a652df3c
Gerdinebehr <gerdine672@revature.net>
2024-09-21 15:21:37 -0400

    logger file began and edited/export

---
+++ src/main/util/logger.js
@@ -0,0 +1,20 @@
+// Logger setup
+const logger = createLogger({
+    level: 'info',
+    format: format.combine(
+        format.timestamp(),
+        format.printf(({ timestamp, level, message }) => `${timestamp} [${level}]: ${message}`)
+    ),
+    transports: [
+        new transports.Console(),
+        new transports.File({ filename: 'app.log' })
+    ]
+});
+/*
+process.on("uncaughtException", (error) => {
+    logger.error(`Uncaught Exception: ${error}`);
+    process.exit(1);
+});
+*/
+
+module.exports = logger;
