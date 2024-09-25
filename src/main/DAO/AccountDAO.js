d5e7b0cd8cc4cf6e78352e3781ff2372ff8cf7fb
Gerdinebehr <gerdine672@revature.net>
2024-09-25 13:13:09 -0400

    added new feature to reject duplicate usernames

--- src/main/DAO/AccountDAO.js
+++ src/main/DAO/AccountDAO.js
@@ -1,9 +1,7 @@
 const http = require('http');
 const { createLogger, transports, format } = require('winston');
 require('dotenv').config();
-const {account} = require('../Model/Model.js'); 
-
-// const accountTest = new account; //JUST A TEST, DELETE AFTER
+const { account } = require('../Model/Model.js');
 
 const logger = createLogger({
     level: 'info',
@@ -18,11 +16,10 @@ const logger = createLogger({
 });
 
 // Setup Database 
-const { DynamoDBClient, QueryCommand, ScanCommand } = require("@aws-sdk/client-dynamodb");
-const { DynamoDBDocumentClient, GetCommand, PutCommand, UpdateCommand, DeleteCommand } = require("@aws-sdk/lib-dynamodb");
+const { DynamoDBClient, ScanCommand } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, GetCommand, PutCommand, QueryCommand } = require("@aws-sdk/lib-dynamodb");
 
 console.log("AWS_REGION: ", process.env.AWS_REGION);
-
 
 // Create a DynamoDB client
 const dynamoDbClient = new DynamoDBClient({
@@ -33,47 +30,67 @@ const dynamoDbClient = new DynamoDBClient({
     }
 });
 
-
 const documentClient = DynamoDBDocumentClient.from(dynamoDbClient);
 
 const TableName = "AccountData";
 
-//set up commands
-
-async function postAccount(account){
+// Function to post a new account to the database
+async function postAccount(account) {
     const command = new PutCommand({
         TableName, 
         Item: account
     });
-    try{
+    try {
         const data = await documentClient.send(command);
         return data;
-
-    } catch (err){
-        logger.error(err);
+    } catch (err) {
+        logger.error("Error creating account:", err);
         throw err;
     }
 }
 
-async function getAccount(){
+// Function to get all accounts from the database
+async function getAccount() {
     const command = new ScanCommand({
-        TableName, 
+        TableName,
     });
-    try{
+    try {
         const data = await documentClient.send(command);
         return data.Account;
-    } catch(err){
-    logger.error(err);
-    throw err;
+    } catch (err) {
+        logger.error("Error fetching accounts:", err);
+        throw err;
     }
 }
 
-module.exports = { 
-    getAccount, 
-    postAccount
+// Function to get an account by username to check uniqueness
+async function getAccountByUsername(username) {
+    const command = new QueryCommand({
+        TableName,
+        IndexName: "Username-index", // Assuming you have a GSI on Username
+        KeyConditionExpression: "#username = :username",
+        ExpressionAttributeNames: {
+            "#username": "Username"
+        },
+        ExpressionAttributeValues: {
+            ":username": username
+        }
+    });
+    try {
+        const data = await documentClient.send(command);
+        return data.Items.length > 0 ? data.Items[0] : null; // Return the first matching account, or null if not found
+    } catch (err) {
+        logger.error("Error fetching account by username:", err);
+        throw err;
+    }
 }
- 
- 
+
+module.exports = {
+    getAccount,
+    postAccount,
+    getAccountByUsername
+};
+
  // USER FACING 
 
 
--- src/main/Service/AccountService.js
+++ src/main/Service/AccountService.js
@@ -1,22 +1,35 @@
 const accountDao = require("../DAO/AccountDAO");
-const uuid = require("uuid"); 
+const uuid = require("uuid");
 
 async function postAccount(account) {
-    // Validate the account
-    if (validateAccount(account)) {
+    // Validate the account including checking for username uniqueness
+    const isValid = await validateAccount(account);
+    if (isValid) {
         // Destructure account and add AccountID
         let data = await accountDao.postAccount({
             ...account, // Spread the account properties
-            AccountID: uuid.v4() // Add unique AccountID using uuid
+            AccountID: uuid.v4(), // Add unique AccountID using uuid
+            Role: account.Role || "Employee" // Set default role to "Employee" if not provided
         });
         return data;
     }
     return null; // Controller layer checks if this is truthy or falsy 
 }; 
 
-function validateAccount(account) {
+async function validateAccount(account) {
     // Ensure both Password and Username exist
-    return (account.Password && account.Username); // Boolean && operator will return false if either are missing
+    if (!account.Password || !account.Username) {
+        return false; // Return false if either password or username is missing
+    }
+
+    // Check if the username already exists in the database
+    const existingAccount = await accountDao.getAccountByUsername(account.Username);
+    if (existingAccount) {
+        console.error("Username already exists:", account.Username); // Log the duplicate username
+        return false; // Return false if the username is already taken
+    }
+
+    return true; // Return true if validation passes
 }
 
 module.exports = {
